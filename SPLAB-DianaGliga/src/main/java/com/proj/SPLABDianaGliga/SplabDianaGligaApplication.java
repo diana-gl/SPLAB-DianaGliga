b6f7b2d0fdd4cfaaffe83d119cea4cfef7104a08
Diana Gliga <diana.c.gliga@gmail.com>
2023-10-30 18:26:51 +0200

    commit unversioned files

---
+++ SPLAB-DianaGliga/src/main/java/com/proj/SPLABDianaGliga/SplabDianaGligaApplication.java
@@ -0,0 +1,12 @@
+package com.proj.SPLABDianaGliga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplabDianaGligaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SplabDianaGligaApplication.class, args);
+	}
+
+}
